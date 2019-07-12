0be2f3ef05b89ef68297c5b2d665997c13722bcb
SebastiaN <odm_sd@hotmail.com>
2019-07-12 18:02:37 -0500

    consulta de commit en github

--- app/Commits.php
+++ app/Commits.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Unirest\Request as Consulta;
 
 class Commits extends Model
 {
@@ -11,4 +12,16 @@ class Commits extends Model
     protected $casts            =       [
         "respuesta_co"=>"object"
     ];
+
+    protected $appends = ['github'];
+
+    public function getGithubAttribute(){
+        Consulta::verifyPeer(false);
+        $response = Consulta::get("https://api.github.com/repos/Inpsercom-IT/kia-personalizado/commits/af9c5483f9e4b2d9be64865fbd556f759353bcac",
+            [
+                "Authorization"     =>  "token  ".env("GITHUB_TOKEN")
+            ]
+        );
+        return $response->body;
+    }
 }
