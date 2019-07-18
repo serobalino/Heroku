88201158ceb3c5fb3fdf575693e65bcb470deb3e
SebastiaN <odm_sd@hotmail.com>
2019-07-18 18:50:47 -0500

    consulta de github por commit

--- app/Http/Controllers/ConsultaCommit.php
+++ app/Http/Controllers/ConsultaCommit.php
@@ -9,21 +9,13 @@ class ConsultaCommit extends Controller
 {
     function serialziacion($app,$commit=null){
         $aplicacion = Apps::where("nombre_app",$app)->first();
+        if($aplicacion){
+            if($commit===null)
+                $commits=$aplicacion->commits;
+            else
+                $commits=$aplicacion->commits()->where('id_co', $commit)->get();
+        }
 
-        if($commit===null)
-            $commits=$aplicacion->commits;
-        else
-            $commits=$aplicacion->commits()->where('id_co', $commit)->get();
-
-        return response($commits);
-    }
-    public function httpreques(){
-        Consulta::verifyPeer(false);
-        $response = Consulta::get("https://api.github.com/repos/Inpsercom-IT/kia-personalizado/commits/af9c5483f9e4b2d9be64865fbd556f759353bcac",
-            [
-                "Authorization"     =>  "token  ".env("GITHUB_TOKEN")
-            ]
-        );
-        return response()->json($response);
+        return response(@$commits,@$commits?200:500);
     }
 }
