565cd46eda43055d0e11694ff6020325468f5158
SebastiaN <sebastian.robalino@inpsercom.com>
2020-09-25 13:29:42 -0500

    noficacion de slack

---
+++ app/Notifications/SlackError.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Commits;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\SlackMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class SlackError extends Notification
+{
+    use Queueable;
+
+    protected $nuevo;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(Commits $nuevo)
+    {
+        $this->nuevo=$nuevo;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['slack'];
+    }
+
+    public function toSlack($notifiable)
+    {
+        $github =   $this->nuevo->github;
+        $url    =   url(route("generar", ["app" => $this->nuevo->app_co, "commit" => $this->nuevo->id_co]));
+        return (new SlackMessage)
+            ->error()
+            ->from($github->committer->login.' ha generado un Error')
+            ->image($github->committer->avatar_url)
+            ->to('#pila-versionamiento')
+            ->content("_".$github->commit->message."_")
+            ->attachment(function ($attachment) use ($url) {
+                $attachment->title("Link: ".$this->nuevo->app_co, $url);
+            });
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+       //
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
