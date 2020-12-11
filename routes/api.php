5702fcb73c566b94a97c538d01eaaebdb3d5c419
SebastiaN <sebastian.robalino@inpsercom.com>
2020-12-11 14:56:18 -0500

    ruta apra generar cedulas validas

--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 //Route::resource("heroku","TwilioController");
 Route::post("heroku","ErroresHookController@herokuEmail");
 Route::post("twilio","TwilioController@responde");
+Route::get("ciGenerator","CiGeneratorController@lista");
 
 Route::post('{app}/{commit?}',"ConsultaCommit@serialziacion");
 
