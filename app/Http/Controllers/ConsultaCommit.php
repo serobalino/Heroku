8ce0eeb3ce2b8efcdb0dd7c4b7465094a470b6f2
Sebastian <odm_sd@hotmail.com>
2021-06-07 11:18:57 -0500

    envio de ultima compilacion

--- app/Http/Controllers/ConsultaCommit.php
+++ app/Http/Controllers/ConsultaCommit.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Apps;
+use App\Commits;
 use Illuminate\Http\Request;
 
 class ConsultaCommit extends Controller
@@ -21,7 +22,8 @@ class ConsultaCommit extends Controller
     function vista($app,$commit=null){
         $aplicacion = Apps::where("nombre_app",$app)->first();
         if($aplicacion){
-            return view("aplicacion-commit",["app"=>$aplicacion,"id"=>$commit]);
+            $last = Commits::where("app_co",$app)->orderBy('created_at', 'desc')->first();
+            return view("aplicacion-commit",["app"=>$aplicacion,"id"=>$commit,"last"=>$last]);
         }else{
             return abort(404);
         }
