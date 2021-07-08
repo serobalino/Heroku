4ded0d8c726aace1d1ed3be220c801831880a534
Sebastian <odm_sd@hotmail.com>
2021-07-08 09:56:27 -0500

    sent to real channel

--- app/Notifications/SlackErrorGh.php
+++ app/Notifications/SlackErrorGh.php
@@ -48,8 +48,7 @@ class SlackErrorGh extends Notification
             ->error()
             ->from(@$github->committer->login.' ha generado un Error')
             ->image(@$github->committer->avatar_url ? $github->committer->avatar_url : ":boom:")
-            ->to('#ips-236-alcance-portal-pagos-web')
-//            ->to('#pila-versionamiento')
+            ->to('#pila-versionamiento')
             ->content("<!here> _".$github->commit->message."_")
             ->attachment(function ($attachment) use ($url,$branch,$github,$action) {
                 $attachment
